c00c823da73e8e39404fef98710ea87372a714cc
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-02-07 23:44:08 +0530

    shell profile modify

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,20 @@
-fnm env --use-on-cd | Out-String | Invoke-Expression
-
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\blueish.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\devious-diamonds.omp.yaml" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\M365Princess.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\dracula.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\atomic.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\emodipt-extend.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\montys.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\wholespace.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin_mocha.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\the-unnamed.omp.json" | Invoke-Expression
-
 Invoke-Expression (&starship init powershell)
 
-#alias lsd="ls"
 Import-Module -Name Terminal-Icons
+
+# aliases
+Set-Alias g git
+Remove-Item Alias:ls -Force
+Set-Alias ls lsd
+Set-Alias c clear
+
+# Function for multi-word commands
+function gs { git status }
+function ga { git add . }
+function gp { git push }
+function gca { git commit -m "$args" }
+function lst { lsd --tree }
+function q { exit }
+function .. { cd .. }
+
+fnm env --use-on-cd | Out-String | Invoke-Expression

7189e338b3a9c4974e1d753c2508160c1f1bef39
Aditya Pandey <adityapandey.inbox@gmail.com>
2025-02-13 17:59:43 +0530

    indentation plugin updated

---
+++ nvim/lua/custom/indent.lua
@@ -0,0 +1,24 @@
+return {
+  "custom.plugins.indent", -- This is just a dummy plugin entry
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "javascript", "typescript", "html", "css", "json", "toml", "lua" },
+      callback = function()
+        vim.bo.shiftwidth = 2
+        vim.bo.tabstop = 2
+        vim.bo.softtabstop = 2
+        vim.bo.expandtab = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        vim.bo.shiftwidth = 4
+        vim.bo.tabstop = 4
+        vim.bo.softtabstop = 4
+        vim.bo.expandtab = true
+      end,
+    })
+  end,
+}

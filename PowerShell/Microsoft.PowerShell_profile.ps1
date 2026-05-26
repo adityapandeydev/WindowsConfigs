042bc4214a4340f34adddb696eccb8a7217adc7d
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-05-26 16:04:03 +0530

    modularize powershell functions

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,15 +17,8 @@ Set-Alias ls lsd
 Set-Alias c clear
 Set-Alias ff fastfetch
 
-# Function for multi-word commands
-function gs { git status }
-function ga { git add . }
-function gpu { git push }
-function gca { git commit -m "$args" }
-function gsca {git commit -S -m "$args"}
-function lst { lsd --tree }
-function q { exit }
-function .. { cd .. }
+# Source functions
+. $PSScriptRoot\functions.ps1
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
 

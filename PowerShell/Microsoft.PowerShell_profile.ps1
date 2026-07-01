1d34fa6c43540ccfbb602f560b32601b3b208587
Aditya Pandey <adityapandey.inbox@gmail.com>
2026-07-01 15:44:03 +0530

    fastfetch change

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -27,3 +27,13 @@ function q { exit }
 function .. { cd .. }
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
+
+# F-14 Tactical Fastfetch startup
+$f14FastfetchRoot = 'C:\Users\AdityaPandey\Documents\Configs\fastfetch\fastfetch-f14'
+$f14FastfetchConfig = Join-Path $f14FastfetchRoot 'config.jsonc'
+
+if ((Get-Command fastfetch -ErrorAction SilentlyContinue) -and
+    (Test-Path -LiteralPath $f14FastfetchConfig)) {
+    $env:FASTFETCH_F14_ROOT = $f14FastfetchRoot
+    fastfetch --config $f14FastfetchConfig
+}
